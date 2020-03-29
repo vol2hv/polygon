6a992537a11433bbd5596baf196fbf26e63df0fc
vol2hv <vol2hv@yandex.ru>
2020-03-29 12:00:23 +0300

    заработала одна таблица без security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,8 @@
 delete from box;
 
-INSERT INTO public.box(
-	id, box_status, box_type, description, name)
-	VALUES (101, 'FREE', 'BIG', 'первый', 'b1'),
-	        (102, 'FREE', 'BIG', 'второйй', 'b2'),
-	        (103, 'FREE', 'BIG', 'третий', 'b3');
+INSERT INTO public.box(id, description, name)
+	VALUES (11, 'первый', 'b1'),
+	        (12, 'второйй', 'b2'),
+	        (13, 'третий', 'b3');
 
 alter sequence hibernate_sequence restart with 100;
