096a6562686976d1aca5e990dc7e189a2e38179f
vol2hv <vol2hv@yandex.ru>
2021-05-11 01:03:15 +0300

    Внедрение  Springом через аннотации с использованием @Autowired

--- src/test/java/com/example/polygon/beanexample/MessageRendererTest.java
+++ src/test/java/com/example/polygon/beanexample/MessageRendererTest.java
@@ -3,23 +3,29 @@ package com.example.polygon.beanexample;
 import com.example.polygon.config.ConfigApp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest()
 class MessageRendererTest {
-
+    @Autowired
+    private MessageRenderer render;
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void render() {
-        ApplicationContext  context = new AnnotationConfigApplicationContext(ConfigApp.class);
-        MessageRenderer renderer = context.getBean("render", MessageRenderer.class);
+        // просто
+        render.render();
+    }
+    @Test
+    void info () {
+        //Более многословно
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigApp.class);
+        MessageRenderer renderer = context.getBean("simpleMessageRenderer", MessageRenderer.class);
         renderer.render();
     }
 }
