da034d19057588bdc7fb628bae3b6a3357d0cca4
vol2hv <vol2hv@yandex.ru>
2021-06-02 23:09:44 +0300

    02.06.2021

--- src/main/java/com/example/polygon/config/ConfigApp.java
+++ src/main/java/com/example/polygon/config/ConfigApp.java
@@ -2,14 +2,17 @@ package com.example.polygon.config;
 
 import com.example.polygon.beanexample.BeanExample;
 import com.example.polygon.beanexample.BeanExampleImpl;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @ComponentScan("com.example.polygon")
 @Configuration
 public class ConfigApp {
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     BeanExample beanExample(){
         return new BeanExampleImpl();
     }
--- src/main/java/com/example/polygon/repository/BoxRepo.java
+++ src/main/java/com/example/polygon/repository/BoxRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BoxRepo extends JpaRepository<Box, Long> {
+//
     Box findByName(String name);
 
     @Query("SELECT b FROM Box b WHERE b.name = ?1")
